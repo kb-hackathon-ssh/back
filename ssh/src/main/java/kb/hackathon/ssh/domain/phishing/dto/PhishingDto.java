203cd712154909fe0f9c167b78182a4e1972f322
kxxheehxxn <fpqk7099@gmail.com>
2025-09-11 21:29:17 +0900

    feat: 보이스피싱

--- ssh/src/main/java/kb/hackathon/ssh/domain/phishing/dto/PhishingDto.java
+++ ssh/src/main/java/kb/hackathon/ssh/domain/phishing/dto/PhishingDto.java
@@ -1,4 +1,40 @@
 package kb.hackathon.ssh.domain.phishing.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
 public class PhishingDto {
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Item {
+        private String id;
+        private String type;
+        private String value;
+        private int reports;
+        private String lastReported;
+        private String source;
+        private String risk;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class LookupResponse {
+        private String ok = "true";
+        private Map<String, String> query;
+        private Map<String, String> meta;
+        private List<Item> items = new ArrayList<>();
+    }
 }
